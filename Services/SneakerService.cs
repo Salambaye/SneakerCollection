7dcf2c6cc5612397e3989b1bcd50c24c4e5028bd
Sala <Salambaye2001@gmail.com>
2025-09-10 19:14:51 +0200

    Add custom Tag Helpers (SneakerCard, ConditionBadge)

--- Services/SneakerService.cs
+++ Services/SneakerService.cs
@@ -115,7 +115,7 @@ namespace SneakerCollection.Services
                     Category = SneakerCategory.Basketball,
                     ReleaseDate = new DateTime(2015, 5, 30),
                     AddedDate = DateTime.Now.AddDays(-30),
-                    ImageUrl = "https://images.stockx.com/images/Air-Jordan-1-Retro-High-Chicago-2015.jpg",
+                    ImageUrl = "https://www.bluedropshop.fr/cdn/shop/products/air-jordan-1-high-chicago-lost-and-found-2.png?v=1701245823&width=1445",
                     Description = "Colorway emblématique de la Air Jordan 1, inspirée de l'équipe des Chicago Bulls.",
                     IsLimited = true,
                     StockQuantity = 2
@@ -140,19 +140,19 @@ namespace SneakerCollection.Services
                 new Sneaker
                 {
                     Id = 3,
-                    Brand = SneakerBrand.Adidas,
-                    Model = "Yeezy Boost 350 V2",
-                    Colorway = "Zebra",
-                    Size = 10.0m,
-                    Price = 80.00m,
+                    Brand = SneakerBrand.NewBalance,
+                    Model = "990v3",
+                    Colorway = "Grey",
+                    Size = 11.0m,
+                    Price = 185.00m,
                     Condition = SneakerCondition.VeryGood,
-                    Category = SneakerCategory.Lifestyle,
-                    ReleaseDate = new DateTime(2017, 2, 25),
-                    AddedDate = DateTime.Now.AddDays(-20),
-                    ImageUrl = "https://www.sneakers.fr/wp-content/uploads/2017/01/adidas-yeezy-boost-350V2-Zebra-8-380x380.jpeg",
-                    Description = "L'un des colorways les plus populaires de la Yeezy 350 V2.",
-                    IsLimited = true,
-                    StockQuantity = 3
+                    Category = SneakerCategory.Running,
+                    ReleaseDate = new DateTime(2012, 8, 15),
+                    AddedDate = DateTime.Now.AddDays(-5),
+                    ImageUrl = "https://img01.ztat.net/article/spp-media-p1/91e8113ecb8b4fa292e3c7add34c2a2b/f214354f749e4122a24d2021461aef40.jpg?imwidth=1800&filter=packshot",
+                    Description = "Sneaker de running premium avec un confort exceptionnel.",
+                    IsLimited = false,
+                    StockQuantity = 2
                 },
                 new Sneaker
                 {
@@ -166,7 +166,7 @@ namespace SneakerCollection.Services
                     Category = SneakerCategory.Lifestyle,
                     ReleaseDate = new DateTime(2021, 3, 10),
                     AddedDate = DateTime.Now.AddDays(-15),
-                    ImageUrl = "https://images.stockx.com/images/Nike-Dunk-Low-White-Black.jpg",
+                    ImageUrl = "https://media.cdn.kaufland.de/product-images/1024x1024/f38b0c65abc3d44201a7a8fdb500a9fc.webp",
                     Description = "Le colorway 'Panda' est devenu un incontournable avec son design noir et blanc classique.",
                     IsLimited = false,
                     StockQuantity = 5
@@ -174,16 +174,17 @@ namespace SneakerCollection.Services
                 new Sneaker
                 {
                     Id = 5,
-                    Brand = SneakerBrand.Jordan,
-                    Model = "Air Jordan 4 Retro",
-                    Colorway = "Black Cat",
+                    Brand = SneakerBrand.Asics,
+                    Model = "Asics Gel-NYC",
+                    Colorway = "Pink Cream Pure Silver",
                     Size = 8.0m,
                     Price = 220.00m,
                     Condition = SneakerCondition.DeadStock,
                     Category = SneakerCategory.Basketball,
                     ReleaseDate = new DateTime(2020, 1, 18),
                     AddedDate = DateTime.Now.AddDays(-10),
-                    ImageUrl = "https://images.stockx.com/images/Air-Jordan-4-Retro-Black-Cat-2020.jpg",
+                    //ImageUrl = "https://www.sneakerstyle.fr/wp-content/uploads/2020/01/air-jordan-4-iv-retro-black-cat-2020-CU1110-010-01.jpg",
+                    ImageUrl = " https://www.globalsneakers.fr/cdn/shop/files/asics_collaboration_2048x.png?v=1727541235",
                     Description = "Version entièrement noire de la mythique Air Jordan 4.",
                     IsLimited = true,
                     StockQuantity = 1
@@ -191,18 +192,20 @@ namespace SneakerCollection.Services
                 new Sneaker
                 {
                     Id = 6,
-                    Brand = SneakerBrand.NewBalance,
-                    Model = "990v3",
-                    Colorway = "Grey",
-                    Size = 11.0m,
-                    Price = 185.00m,
+                    Brand = SneakerBrand.Adidas,
+                    Model = "Campus HQ8708",
+                    Colorway = "Black",
+                    Size = 10.0m,
+                    Price = 80.00m,
                     Condition = SneakerCondition.VeryGood,
-                    Category = SneakerCategory.Running,
-                    ReleaseDate = new DateTime(2012, 8, 15),
-                    AddedDate = DateTime.Now.AddDays(-5),
-                    Description = "Sneaker de running premium avec un confort exceptionnel.",
-                    IsLimited = false,
-                    StockQuantity = 2
+                    Category = SneakerCategory.Lifestyle,
+                    ReleaseDate = new DateTime(2017, 2, 25),
+                    AddedDate = DateTime.Now.AddDays(-20),
+                    ImageUrl = "https://www.sneakerstyle.fr/wp-content/uploads/2023/08/adidas-campus-00s-core-black-HQ8708-01.webp",
+                    Description = "L'un des colorways les plus populaires de la Yeezy 350 V2.",
+                    IsLimited = true,
+                    StockQuantity = 3
+
                 },
                 new Sneaker
                 {
@@ -216,6 +219,7 @@ namespace SneakerCollection.Services
                     Category = SneakerCategory.Skateboarding,
                     ReleaseDate = new DateTime(1977, 3, 19),
                     AddedDate = DateTime.Now.AddDays(-3),
+                    ImageUrl = "https://photos6.spartoo.com/photos/485/4858117/4858117_350_A.jpg",
                     Description = "La sneaker de skate iconique de Vans, un classique intemporel.",
                     IsLimited = false,
                     StockQuantity = 10
@@ -232,6 +236,7 @@ namespace SneakerCollection.Services
                     Category = SneakerCategory.Lifestyle,
                     ReleaseDate = new DateTime(1982, 12, 1),
                     AddedDate = DateTime.Now.AddDays(-1),
+                    ImageUrl = "https://cdn.lesitedelasneaker.com/wp-content/images/2022/04/nike-air-force-1-low-07-triple-white-cw2288-111-pic02-1100x825.jpg",
                     Description = "La sneaker la plus vendue de Nike, un incontournable en version blanche.",
                     IsLimited = false,
                     StockQuantity = 8
