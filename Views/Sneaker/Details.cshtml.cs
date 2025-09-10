3f660d4c828df3596c67404cda6f7ae2a6987a13
Sala <Salambaye2001@gmail.com>
2025-09-10 14:54:39 +0200

    Add Sneaker CRUD views (Index, Details, Create, Edit, Delete)

---
+++ Views/Sneaker/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SneakerCollection.Views.Sneaker
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
